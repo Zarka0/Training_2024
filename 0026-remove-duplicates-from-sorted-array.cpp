4756bd86b79d8efb60fd01f7b7c1d5fb467b6578
Zarka0 <95235087+Zarka0@users.noreply.github.com>
2024-07-22 20:26:33 +0530

    Add files via upload

---
+++ 0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+ set<int> s1(nums.begin(), nums.end());
+ nums=vector<int>(s1.begin(), s1.end());
+ return s1.size();
+}
+};
---
+++ 0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        if (prices.empty()) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = std::max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+};
